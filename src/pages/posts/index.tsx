e49dc9e4765fe92e5583b2773b52f376dc65e02d
Diego Oliveira <dev@HOME_PC.localdomain>
2022-07-31 22:06:58 -0300

    feat prismic request

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import * as prismic from '@prismicio/client';
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -31,4 +34,22 @@ export default function Posts() {
 
 
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const prismicClient = getPrismicClient()
+
+  const response = await prismicClient.get({
+    predicates: [prismic.predicate.at('document.type', 'post')],
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+
+  console.log(JSON.stringify(response, null, 2))
+
+  return {
+    props: {}
+  }
 }
---
+++ src/services/prismic.ts
@@ -0,0 +1,12 @@
+import * as prismic from '@prismicio/client';
+
+export function getPrismicClient(req?: unknown) {
+    const prismicClient = prismic.createClient(process.env.PRISMIC_ENDPOINT);
+
+    prismicClient.accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+
+    req && prismicClient.enableAutoPreviewsFromReq(req);
+
+
+    return prismicClient;
+}
